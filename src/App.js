24264e653808b44ab150a3dac60d801b3bca1d3f
jatinlalwani97 <46535630+jatinlalwani97@users.noreply.github.com>
2020-03-24 14:42:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+class App extends Component {
+  render(){
+    return <h>Hello from Tech store</h>
+  }
+}
+
+export default App;
